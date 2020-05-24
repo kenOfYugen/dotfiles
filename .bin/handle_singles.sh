03b5a0514be95d7dde62d0db1eed52fca807deaa
Gokul Swaminathan <gokulswamilive@gmail.com>
2020-05-24 14:40:20 -0700

    new changes and spectrwm setup

--- .bin/handle_singles.sh
+++ .bin/handle_singles.sh
@@ -3,8 +3,10 @@
 bspc subscribe | while read -r line; do
     node_num=$(bspc query --nodes --desktop | wc -l)
     if [ "$node_num" == "1" ]; then
-        bspc config border_width 0	
+        bspc config border_width 0
+        bspc config window_gap   0
     else
-        bspc config border_width 4
+        bspc config border_width 3
+        bspc config window_gap   5
     fi
 done;
