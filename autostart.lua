9210541e64abe7dad9c58b130ebdf593e4f41d9d
Gokul Swaminathan <gokulswamilive@gmail.com>
2020-12-27 12:36:48 -0800

    Changing dotfiles to awesome-config

---
+++ autostart.lua
@@ -0,0 +1,44 @@
+-- autostart.lua
+-- Autostart Stuff Here
+local awful = require("awful")
+local gears = require("gears")
+
+local function run_once(cmd)
+    local findme = cmd
+    local firstspace = cmd:find(' ')
+    if firstspace then findme = cmd:sub(0, firstspace - 1) end
+    awful.spawn.with_shell(string.format(
+                               'pgrep -u $USER -x %s > /dev/null || (%s)',
+                               findme, cmd), false)
+end
+
+-- Network Manager Applet
+run_once("nm-applet")
+
+-- Disable Bell
+run_once("xset -b")
+
+-- Mpd Cleanup
+run_once([[
+    ps aux | grep "mpc idleloop player" | grep -v grep | awk '{print $2}' | xargs kill
+    ]])
+
+-- For bitmap
+run_once([[
+    xset +fp /home/javacafe01/.local/share/fonts; xset fp rehash
+]])
+
+-- Bluetooth
+run_once("blueman-applet")
+
+-- For Dual Monitors
+run_once(
+    "if xrandr | grep DP-1-0; then xrandr --auto --output DP-1-0 --mode 1920x1080 --right-of eDP1; fi")
+
+-- Compositor
+run_once("picom --experimental-backends --config " ..
+             gears.filesystem.get_configuration_dir() .. "configs/picom.conf")
+
+return autostart
+
+-- EOF ------------------------------------------------------------------------
---
+++ bloat/pop/notif.lua
@@ -0,0 +1,33 @@
+-- notif.lua
+-- Notification Popup Widget
+local awful = require("awful")
+local gears = require("gears")
+local wibox = require("wibox")
+local beautiful = require("beautiful")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+
+local popupLib = require("utils.popupLib")
+
+local popupWidget = wibox.widget {
+    {
+        require("notifs.notif-center"),
+        margins = dpi(8),
+        widget = wibox.container.margin
+    },
+    expand = "none",
+    layout = wibox.layout.fixed.horizontal
+}
+
+local width = 400
+local margin = 10
+
+local popup = popupLib.create(awful.screen.focused().geometry.width - width,
+                              beautiful.wibar_height, nil, width, popupWidget,
+                              dpi(25), false, false, false, true)
+
+popup:set_xproperty("WM_NAME", "notif-center")
+
+return popup
+
+-- EOF ------------------------------------------------------------------------
